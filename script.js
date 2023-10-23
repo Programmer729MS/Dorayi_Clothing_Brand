0463cd183eb1496e28ddd5df6a9326acad5d52d4
Muhammad <muhammadsalisuhasaan729@gmail.com>
2023-10-23 03:48:20 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function scrollToTop(){
+    window.scrollTo(0,0);
+}
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
